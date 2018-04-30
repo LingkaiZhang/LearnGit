69f7765e2a893fa79ffd0fb5b212154cd8b3eb4c
lingkai <382695973@qq.com>
2018-04-30 22:11:47 +0800

    加载等待指示器

--- ch04/more.js
+++ ch04/more.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
 
 type Props = {};
 export default class more extends Component<Props> {
   render() {
     return(
       <View style={styles.container}>
+        <ActivityIndicator color="purple" size="large"/>
         <Text style={styles.text}>
           更多页面
         </Text>
